a50e9eaf5b5fe8ca7155fe5b4347d56e19e4f00b
Wander D <wd.delossantos@gmail.com>
2025-10-30 17:11:43 -0400

    first commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import "../global.css";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { Slot } from 'expo-router'
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Slot />
+    </ClerkProvider>
+  )
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Page() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Welcome to the Home Page!</Text>
+    </View>
+  );
+}
+
